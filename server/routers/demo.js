33f84c890cd833df4c144395bb15829dd1e2fe80
Sudharaka <sudharaka@service-link.us>
2018-05-30 22:55:35 -0700

    Configure Handlebars.js to dynamic email templating

--- server/routers/demo.js
+++ server/routers/demo.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require("fs");
 const nodemailer = require('nodemailer');
 const mailConfig = require('../config/mail-config.json');
+const handlebars = require('handlebars');
+var compiledTemplate = '';
 
 module.exports = function (app, basepath) {
   app.use(bodyParser.json()); // for parsing application/json
-  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({
+    extended: true
+  })); // for parsing application/x-www-form-urlencoded
   var router = express.Router();
+
+  if (compiledTemplate == '') {
+    fs.readFile("server/config/email-templates/email-template.html", "utf8", function (err, data) {
+      if (err) throw err;
+      let template = data.toString();
+      compiledTemplate = handlebars.compile(template);
+    });
+  }
 
   router.post('/demo', function (req, res, next) {
     let transporter = nodemailer.createTransport({
-      host: mailConfig.smtpHost, 
+      host: mailConfig.smtpHost,
       port: mailConfig.smtpPort,
       secure: mailConfig.smtpSSL, // true for 465, false for other ports
       auth: {
@@ -19,22 +32,26 @@ module.exports = function (app, basepath) {
       }
     });
 
+    let emailMessage = compiledTemplate({
+      "name": req.body.name
+    })
+
     // setup email data with unicode symbols
-    let mainMessageCus = {
+    let mailMessageCus = {
       from: '"Service-Link" <' + mailConfig.from + '>', // sender address
       to: req.body.email, // list of receivers
       subject: 'Demo Request: Service-Link', // Subject line
       text: 'Dear ' + req.body.name + ', We are excited to hear your interest in Service-Link and there\'s so much that we would like to share with you. A representative from Service-Link will contact you shortly for more information about the demo. Thank you, Service-Link Team.', // plain text body
-      html: 'Dear ' + req.body.name + ', We are excited to hear your interest in Service-Link and there\'s so much that we would like to share with you. A representative from Service-Link will contact you shortly for more information about the demo. Thank you, Service-Link Team.' // html body
+      html: emailMessage // html body
     };
 
     // setup email data with unicode symbols
-    let mainMessageSup = {
+    let mailMessageSup = {
       from: '"Service-Link" <' + mailConfig.from + '>', // sender address
       to: mailConfig.SLSupport, // list of receivers
-      subject: 'Demo Request: Service-Link', // Subject line
-      text: 'The following request has been recieved for a demo request via the Service-Link webpage. Name: ' + req.body.name + 'Company: ' + req.body.company + 'Email: ' + req.body.email + 'Country: ' + req.body.country, // plain text body
-      html: 'The following request has been recieved for a demo request via the Service-Link webpage.<br>Name: ' + req.body.name + '<br>' + 'Company: ' + req.body.company + '<br>' + 'Email: ' + req.body.email + '<br>' + 'Country: ' + req.body.country // html body
+      subject: 'Demo Request by ' + req.body.name + ': Service-Link', // Subject line
+      text: 'Hi there, The following customer has requested for a demo request via the Service-Link webpage. Name: ' + req.body.name + 'Company: ' + req.body.company + 'Email: ' + req.body.email + 'Country: ' + req.body.country, // plain text body
+      html: 'Hi there, <br><br>The following request has been recieved for a demo request via the Service-Link webpage.<br><br>Name: ' + req.body.name + '<br><br>' + 'Company: ' + req.body.company + '<br><br>' + 'Email: ' + req.body.email + '<br><br>' + 'Country: ' + req.body.country // html body
     };
 
     // Send mail to support
@@ -43,7 +60,9 @@ module.exports = function (app, basepath) {
         return console.log(error);
       }
       console.log('Message sent: %s', info.messageId);
-      res.json({status: info.response});
+      res.json({
+        status: info.response
+      });
     });
 
     // Send mail to customer
@@ -52,7 +71,9 @@ module.exports = function (app, basepath) {
         return console.log(error);
       }
       console.log('Message sent: %s', info.messageId);
-      res.json({status: info.response});
+      res.json({
+        status: info.response
+      });
     });
   })
 
