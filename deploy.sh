1a7a8e04fa055fab42c59e24e5b2e3fb21ae9033
Sudharaka <sudharaka@service-link.us>
2018-06-11 11:53:27 -0700

    Deployment through google app engine

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+REPO="https://github.com/SudharakaP/Service-Link-Angular.git"
+
+# Clone the existing Service-Link-Angular front-end
+cd .. && git clone $REPO && cd Service-Link-Angular
+
+# Run the install and build and copy the files over
+npm run install
+npm run build
+cp -rf dist/* ../ServiceLinkNode/public/
+
+cd ../ServiceLinkNode
+
+# Decrypt the key file
+openssl aes-256-cbc -K $encrypted_76bf75918cf2_key -iv $encrypted_76bf75918cf2_iv -in gcloudkey.json.enc -out gcloudkey.json -d
