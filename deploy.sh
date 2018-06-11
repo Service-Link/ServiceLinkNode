c90259272eaab687df8eeefe555644c8c488a03c
Sudharaka <sudharaka@service-link.us>
2018-06-11 16:07:34 -0700

    Forgot to move the files after decryption

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,4 @@ cd ../ServiceLinkNode
 # Decrypt the key file
 openssl aes-256-cbc -K $encrypted_5cc90922a0ab_key -iv $encrypted_5cc90922a0ab_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
+mv log4js.json mail-config.json server/config/
