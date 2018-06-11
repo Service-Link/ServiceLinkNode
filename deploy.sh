dd01111e90aac714cdad599b5cc2d09f4e7e8381
Sudharaka <sudharaka@service-link.us>
2018-06-11 13:04:57 -0700

    Regenerate encryption key and minor change to deployment

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ cd .. && git clone $REPO && cd Service-Link-Angular
 
 # Run the install and build and copy the files over
 
-npm install && npm rebuild
+npm install
 npm run build
 cp -rf dist/* ../ServiceLinkNode/public/
 
 cd ../ServiceLinkNode
 
 # Decrypt the key file
-openssl aes-256-cbc -K $encrypted_76bf75918cf2_key -iv $encrypted_76bf75918cf2_iv -in gcloudkey.json.enc -out gcloudkey.json -d
+openssl aes-256-cbc -K $encrypted_5cc90922a0ab_key -iv $encrypted_5cc90922a0ab_iv -in gcloudkey.json.enc -out gcloudkey.json -d
