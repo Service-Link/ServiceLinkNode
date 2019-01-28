6f0891f8a9599a60521559a3d154291024af67c6
Sudharaka <sudharaka@service-link.us>
2019-01-27 23:49:33 -0800

    Updating the config files to change the logging in par wtih GCP

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ cp -rf dist/* ../ServiceLinkNode/public/
 cd ../ServiceLinkNode
 
 # Decrypt the key file
-openssl aes-256-cbc -K $encrypted_5cc90922a0ab_key -iv $encrypted_5cc90922a0ab_iv -in secrets.tar.enc -out secrets.tar -d
+openssl aes-256-cbc -K $encrypted_cf2799375742_key -iv $encrypted_cf2799375742_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
